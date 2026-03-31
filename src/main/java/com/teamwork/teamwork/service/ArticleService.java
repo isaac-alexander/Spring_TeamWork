59bbf1643dbf2c2756e444b8be586bebede2afea
Isaac Alexander <Isaacalexander81@gmail.com>
2026-03-31 02:02:47 +0100

    src/main/java/com/teamwork/teamwork/service/ArticleService.java

--- src/main/java/com/teamwork/teamwork/service/ArticleService.java
+++ src/main/java/com/teamwork/teamwork/service/ArticleService.java
@@ -6,8 +6,13 @@ import java.util.List;
 
 public interface ArticleService {
 
-    void saveArticle(Article article);
+    void saveArticle(Article article);           // Create or update an article
 
-    List<Article> getAllArticles();
+    List<Article> getAllArticles();              // List all articles (newest first)
 
+    void deleteArticle(Long id);                 // Delete article by id
+
+    Article getArticleById(Long id);             // Get article by id
+
+    void addComment(Long articleId, String comment); // Add a comment to article
 }
